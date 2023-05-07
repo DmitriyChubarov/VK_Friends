2577b6d1704456d1bf0d677ab5158cf76e7e7273
Dmitriy Chubarov <cubarrv@gmail.com>
2023-05-07 20:05:42 +0300

    rec

--- VKFriends/main/views.py
+++ VKFriends/main/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 def main(request):
-    return render(request, 'main/home.html')
+    users = User.objects.exclude(id=request.user.id)
+
+    context = {
+        'users': users
+    }
+
+    return render(request, 'main/home.html', context)
+
+
